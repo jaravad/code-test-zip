7518fd682052356a07becbbf5f409b9a75284a8c
Jesus Santiago <jesusdavid2607@gmail.com>
2024-02-21 18:30:21 -0500

    Add helper function, add error message

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
 const form = document.getElementById('zip-form');
-const outputContainer = document.getElementById('output');
 const container = document.getElementById('container');
+const input = document.getElementById('zip');
+const outputContainer = createElement({
+  type: 'div',
+  className: 'output-container',
+});
 
 form.addEventListener('submit', async (event) => {
   try {
     event.preventDefault();
     const formData = new FormData(event.target);
     const zipCode = formData.get('zip');
+    if (zipCode === '') return;
+    input.value = '';
+    outputContainer.innerHTML = '';
+    let errors = document.querySelectorAll('.error');
+    errors.forEach(function (element) {
+      element.remove();
+    });
     const fetchUrl = `https://api.zippopotam.us/us/${zipCode}`;
     const data = await fetch(fetchUrl);
     const parsedData = await data.json();
 
     // Display data
-    const outputContainer = document.createElement('div');
-    outputContainer.className = 'output-container';
-    const countryHeader = document.createElement('h2');
-    countryHeader.className = 'country-header';
-    countryHeader.textContent = `${parsedData['post code']} ${parsedData['country']} (${parsedData['country abbreviation']})`;
+    const countryHeader = createElement({
+      type: 'h2',
+      className: 'country-header',
+      text: `${parsedData['post code']} ${parsedData['country']} (${parsedData['country abbreviation']})`,
+    });
+    const placesSubtitle = createElement({
+      type: 'h4',
+      className: 'places-subtitle',
+      text: 'Places:',
+    });
     outputContainer.appendChild(countryHeader);
-    const placesSubtitle = document.createElement('h4');
-    placesSubtitle.textContent = 'Places:';
-    placesSubtitle.className = 'places-subtitle';
     outputContainer.appendChild(placesSubtitle);
     parsedData.places.map((place, i) => {
-      const placeDiv = document.createElement('div');
-      placeDiv.className = 'place-container';
-      const textDiv = document.createElement('div');
-      textDiv.className = 'place-text-container';
-      const placeNameSubtitle = document.createElement('h3');
-      placeNameSubtitle.className = 'place-name-subtitle';
-      placeNameSubtitle.textContent = place['place name'];
+      const placeDiv = createElement({
+        type: 'div',
+        className: 'place-container',
+      });
+      const textDiv = createElement({
+        type: 'div',
+        className: 'place-text-container',
+      });
+      const placeNameSubtitle = createElement({
+        type: 'h3',
+        className: 'place-name-subtitle',
+        text: place['place name'],
+      });
       textDiv.appendChild(placeNameSubtitle);
       const stateName = document.createElement('h4');
       stateName.textContent = `${place.state} (${place['state abbreviation']})`;
@@ -39,21 +58,37 @@ form.addEventListener('submit', async (event) => {
       placeDiv.appendChild(textDiv);
       placeDiv.appendChild(stateImg);
 
-      const longitude = document.createElement('h5');
-      longitude.textContent = 'Longitude: ' + place.longitude;
-      const latitude = document.createElement('h5');
-      latitude.textContent = 'Latitude: ' + place.latitude;
+      const longitude = createElement({
+        type: 'h5',
+        text: `Longitude: ${place.longitude}`,
+      });
+      const latitude = createElement({
+        type: 'h5',
+        text: `Latitude: ${place.latitude}`,
+      });
       textDiv.appendChild(longitude);
       textDiv.appendChild(latitude);
       outputContainer.appendChild(placeDiv);
       if (i < parsedData.places.length - 1) {
-        const divider = document.createElement('hr');
-        divider.className = 'divider';
+        const divider = createElement({ type: 'hr', className: 'divider' });
         outputContainer.appendChild = divider;
       }
     });
     container.appendChild(outputContainer);
   } catch (e) {
+    const errorDiv = createElement({
+      type: 'div',
+      className: 'error',
+      text: 'An error has ocurred',
+    });
+    container.appendChild(errorDiv);
     console.log(e);
   }
 });
+
+function createElement({ type, className, text }) {
+  const element = document.createElement(type);
+  element.className = className;
+  element.textContent = text;
+  return element;
+}
