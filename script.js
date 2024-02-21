05684b205777468657321371b85e02d8dc411145
Jesus Santiago <jesusdavid2607@gmail.com>
2024-02-21 17:47:58 -0500

    Improve styles

--- script.js
+++ script.js
@@ -23,23 +23,34 @@ form.addEventListener('submit', async (event) => {
     placesSubtitle.className = 'places-subtitle';
     outputContainer.appendChild(placesSubtitle);
     parsedData.places.map((place, i) => {
+      const placeDiv = document.createElement('div');
+      placeDiv.className = 'place-container';
+      const textDiv = document.createElement('div');
+      textDiv.className = 'place-text-container';
       const placeNameSubtitle = document.createElement('h3');
       placeNameSubtitle.className = 'place-name-subtitle';
       placeNameSubtitle.textContent = place['place name'];
-      outputContainer.appendChild(placeNameSubtitle);
+      textDiv.appendChild(placeNameSubtitle);
       const stateName = document.createElement('h4');
       stateName.textContent = `${place.state} (${place['state abbreviation']})`;
       const stateImg = document.createElement('img');
       stateImg.src = `./states/${place['state abbreviation']}.svg`;
-      outputContainer.appendChild(stateName);
-      outputContainer.appendChild(stateImg);
+      textDiv.appendChild(stateName);
+      placeDiv.appendChild(textDiv);
+      placeDiv.appendChild(stateImg);
 
       const longitude = document.createElement('h5');
       longitude.textContent = 'Longitude: ' + place.longitude;
       const latitude = document.createElement('h5');
       latitude.textContent = 'Latitude: ' + place.latitude;
-      outputContainer.appendChild(longitude);
-      outputContainer.appendChild(latitude);
+      textDiv.appendChild(longitude);
+      textDiv.appendChild(latitude);
+      outputContainer.appendChild(placeDiv);
+      if (i < parsedData.places.length - 1) {
+        const divider = document.createElement('hr');
+        divider.className = 'divider';
+        outputContainer.appendChild = divider;
+      }
     });
     container.appendChild(outputContainer);
   } catch (e) {
